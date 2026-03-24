a037576b3a579e9a43630a1f48ba6ac55c235ad5
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-03-24 11:16:58 +0200

    Set and retrieve multiple Redis hashes concurrently instead of a single one.

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -2,15 +2,25 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const run = async () => {
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
 		year: 1950
 	});
-	const car = await client.hGetAll('car');
-	if (Object.keys(car).length === 0) {
-		console.log('Car nout found, respond with 404');
-		return;
-	}
-	console.log(car);
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
+
+	const results = await Promise.all(commands);
+
+	console.log(results);
 };
 run();
